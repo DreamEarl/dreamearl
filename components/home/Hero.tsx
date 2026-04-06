23bdfa61301b1348ef6040482c757b99c5606128
Saloni Agrawal <saloniagrawal250797@gmail.com>
2026-04-06 19:01:36 +0530

    feat: Replace SVG icons with Lucide React components for Cart, Menu, and User icons

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
       {/* Background Image */}
       <div className="absolute inset-0">
         <Image
-          src="/background.jpeg"
+          src="/images/hero/background.jpeg"
           alt="Pearl handbag hero image"
           fill
           sizes="100vw"
