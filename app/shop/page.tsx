04dab0bec6066150d8a8faa050b9744dd93d017a
Saloni Agrawal <saloniagrawal250797@gmail.com>
2026-05-24 19:48:26 +0530

    fix: improve null safety in ShopPage component and ensure proper product fetching

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,5 +1,5 @@
 import ProductCard from "@/components/products/ProductCard";
-import { getProductsByCategory, urlFor } from "@/lib/sanity";
+import { getAllProducts, getProductsByCategory, urlFor } from "@/lib/sanity";
 import Heading from "@/components/ui/Heading";
 
 export default async function ShopPage({
@@ -8,21 +8,20 @@ export default async function ShopPage({
   searchParams: Promise<{ category?: string }>;
 }>) {
   const { category: categorySlug } = await searchParams;
-  console.log("🚀 ~ ShopPage ~ categorySlug:", categorySlug);
 
   // Fetch products based on category filter
   const sanityProducts = categorySlug
     ? await getProductsByCategory(categorySlug)
-    : [];
+    : await getAllProducts();
 
   const products = sanityProducts.map((product) => ({
-    id: product._id,
-    name: product.name,
-    price: product.price,
-    image: urlFor(product.images[0]).width(800).url(),
-    href: `/products/${product.slug.current}`,
-    brand: product.brand,
-    currency: product.currency,
+    id: product?._id,
+    name: product?.name,
+    price: product?.price,
+    image: urlFor(product?.images?.[0]).width(800).url(),
+    href: `/products/${product?.slug?.current}`,
+    brand: product?.brand,
+    currency: product?.currency,
   }));
 
   return (
@@ -30,20 +29,20 @@ export default async function ShopPage({
       <main className="pt-32 px-6 md:px-12 max-w-7xl mx-auto pb-16">
         <Heading variant="page" className="text-center mb-12">
           {categorySlug
-            ? categorySlug.charAt(0).toUpperCase() + categorySlug.slice(1)
+            ? categorySlug?.charAt(0).toUpperCase() + categorySlug?.slice(1)
             : "Shop"}
         </Heading>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-8 gap-y-12">
-          {products.map((product) => (
+          {products?.map((product) => (
             <ProductCard
-              key={product.id}
-              id={product.id}
-              name={product.name}
-              price={product.price}
-              image={product.image}
-              href={product.href}
-              brand={product.brand}
-              currency={product.currency}
+              key={product?.id}
+              id={product?.id}
+              name={product?.name}
+              price={product?.price}
+              image={product?.image}
+              href={product?.href}
+              brand={product?.brand}
+              currency={product?.currency}
             />
           ))}
         </div>
